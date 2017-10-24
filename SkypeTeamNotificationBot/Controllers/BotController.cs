992136b96856e12278cb51d9bbf5cc1c8716e130
L0ndra <illialubenets@yandex.ua>
2017-10-24 22:34:40 +0300

    init solution

---
+++ SkypeTeamNotificationBot/Controllers/BotController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Connector;
+using Microsoft.Extensions.Configuration;
+
+namespace SkypeTeamNotificationBot.Controllers
+{
+    [Route("bot")]
+    public class BotController : Controller
+    {
+        private readonly IConfigurationRoot _configuration;
+
+        public BotController(IConfigurationRoot configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        [Authorize(Roles = "Bot")]
+        // POST api/values
+        [HttpPost]
+        public virtual async Task<OkResult> Post([FromBody]Activity activity)
+        {
+            var appCredentials = new MicrosoftAppCredentials(this._configuration);
+            var client = new ConnectorClient(new Uri(activity.ServiceUrl), appCredentials);
+            var reply = activity.CreateReply();
+            if (activity.Type == ActivityTypes.Message)
+            {
+                reply.Text = $"echo: {activity.Text}";
+            }
+            else
+            {
+                reply.Text = $"activity type: {activity.Type}";
+            }
+            await client.Conversations.ReplyToActivityAsync(reply);
+            return Ok();
+        }
+
+    }
+}
---
+++ SkypeTeamNotificationBot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace SkypeTeamNotificationBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webHost = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+            
+            webHost.Run();
+        }
+
+    }
+}
